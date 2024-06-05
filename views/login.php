1b78d4e3b85bbee1cb0c317c55b30392053c3b45
LgGoga <159908724+LgDoga@users.noreply.github.com>
2024-06-05 14:33:33 +0300

    Add files via upload

---
+++ views/login.php
@@ -0,0 +1,18 @@
+<div>
+    <a class="btn btn-light" href="/index.php" role="button">Back to Home Page</a>
+</div>
+<div class="row no-gutters align-items-center justify-content-center text-center h-100">
+    <div class="col-md-6">
+        <form class="form-signin" method="post">
+            <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+            <div class="form-group">
+                <label for="inputEmail" class="sr-only">Login</label>
+                <input type="text" name="login" id="inputEmail" class="form-control" placeholder="Login" autofocus required>
+            </div>
+            <div class="form-group">
+                <label for="inputPassword" class="sr-only">Password</label>
+                <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+            </div>
+        </form>
+    </div>
+</div>
